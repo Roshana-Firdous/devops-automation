4a4820ef231ea62dfa85456baba5d90b054b980b
Abdul Kareem <rajab8321@gmail.com>
2025-02-15 12:15:53 +0530

    version 1.0.0

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("Hello, IBM Cloud DevOps Automation!");
+});
+
+const port = process.env.PORT || 8080;
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
